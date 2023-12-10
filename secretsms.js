c61a92a3e8eec557b5c2b1a1127e6979d7c6a4c9
Nkwada Nora Solonko <nkwadanora@mail.com>
2023-12-09 20:15:33 -0800

    linter errors

--- secretsms.js
+++ secretsms.js
@@ -4,7 +4,7 @@ const encodedMessageDiv = document.getElementById('encoded-message')
 const decodedTextDiv = document.getElementById('decoded-text')
 const encodeBtn = document.getElementById('encode-btn')
 
-encodeBtn.addEventListener('click', function() {
+encodeBtn.addEventListener ('click', function() {
   const sentence = sentenceInput.value.trim() // The .trim() method is then called on the value to remove any leading or trailing whitespace.
 
   if (sentence.length < 50) {
@@ -12,7 +12,7 @@ encodeBtn.addEventListener('click', function() {
     return
   }
 
-  const normalizedText = normalizeText(sentence) //this line calls the normalizeText fxt
+  const normalizedText = normalizeText (sentence) // this line calls the normalizeText fxt
   const encodedMessage = encodeMessage(normalizedText)
   const decodedText = decodeMessage(encodedMessage)
 
@@ -21,18 +21,18 @@ encodeBtn.addEventListener('click', function() {
   decodedTextDiv.textContent = `Decoded Text: ${decodedText}`
 })
 
-function normalizeText(sentence) {
+function normalizeText (sentence) {
   return sentence.replace(/\s/g, '').toLowerCase()
 }
 
-function encodeMessage(text) {
+function encodeMessage (text) {
   const length = text.length
-  const columns = Math.ceil(Math.sqrt(length)) //calculates the number of columns required for the square pattern by taking the ceiling of the square root of the length of the text. This determines the number of characters in each column of the square.
+  const columns = Math.ceil(Math.sqrt(length)) // calculates the number of columns required for the square pattern by taking the ceiling of the square root of the length of the text. This determines the number of characters in each column of the square.
   let encodedMessage = '' // initializes an empty string variable called encodsms to store the encodedsms
 
-  for (let i = 0; i < columns; i++) {  //outer loop
-    for (let j = i; j < length; j += columns) {   //This inner loop iterates over the rows for each column. It starts at the current column index i and increments by columns in each iteration. This ensures that the characters are extracted column-wise from the text.
-      encodedMessage += text[j]  //the character at index j in the text is appended to the encodedMessage string.
+  for (let i = 0; i < columns; i++) {  // outer loop
+    for (let j = i; j < length; j += columns) {   // This inner loop iterates over the rows for each column. It starts at the current column index i and increments by columns in each iteration. This ensures that the characters are extracted column-wise from the text.
+      encodedMessage += text[j]  // the character at index j in the text is appended to the encodedMessage string.
     }
     encodedMessage += ' '  // After each column is processed, a space character is appended to the encodedMessage string to separate the columns.
   }
@@ -40,7 +40,7 @@ function encodeMessage(text) {
   return encodedMessage.trim()
 }
 
-function decodeMessage(encodedMessage) {
+function decodeMessage (encodedMessage) {
   const encodedWords = encodedMessage.split(' ')
   const columns = encodedWords[0].length // This line calculates the number of columns in the square pattern by taking the length of the first word in the encodedWords array
   let decodedText = ''
